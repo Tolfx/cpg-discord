519a915187080f42f319cd45408bc42a9946543e
Tolfx <matteus@tolfx.dev>
2022-03-03 14:31:47 +0100

    Small changes

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,10 @@ const BuildDir = process.cwd() + "/build";
 import type { ILoggingTypes } from "@cpg/Interfaces/Logging.interface";
 // @ts-ignore
 import type mainEvent from "@cpg/Events/Main.event";
+// @ts-ignore
+import type TcreatePDFInvoice from "@cpg/Lib/Invoices/CreatePDFInvoice";
 import config from "../config.json";
-import { WebhookClient } from "discord.js";
+import { WebhookClient, MessageEmbed, MessageAttachment } from "discord.js";
 const discord_webhook_url = process.env.DISCORD_WEBHOOK_URL ?? "";
 
 // Change name of the class.
@@ -13,6 +15,7 @@ export = async function main()
 {
     const Logger = (await import(`${BuildDir}/Lib/Logger`)).default as ILoggingTypes;
     const MainEvent = (await import(`${BuildDir}/Events/Main.event`)).default as typeof mainEvent;
+    const createPDFInvoice = (await import(`${BuildDir}/Lib/Invoices/CreatePDFInvoice`)).default as typeof TcreatePDFInvoice;
 
     if(!discord_webhook_url)
         return Logger.plugin("Discord webhook url not set");
@@ -23,34 +26,84 @@ export = async function main()
 
     Logger.info(`Starting ${config.name} plugin with version ${config.version}.`);
 
-    MainEvent.on("invoice_paid", invoice => {
+    MainEvent.on("invoice_paid", async invoice => {
+        let pdf = await createPDFInvoice(invoice);
+        let buffer = Buffer.from(pdf, "base64");
+        let attachment = new MessageAttachment(buffer, `invoice_${invoice.id}.pdf`);
+        const embed = new MessageEmbed()
+            .setTitle("Invoice notified")
+            .setDescription(`Invoice \`id #${invoice.id}\` has been paid.`)
+            .setColor("#0099ff")
+            .attachFiles([attachment])
+            .setTimestamp();
         sendWebhook("Invoice paid", {
             text: `Invoice \`id #${invoice.id}\` has been paid.`,
+            embeds: [embed],
         })
     });
 
-    MainEvent.on("invoice_created", invoice => {
+    MainEvent.on("invoice_created", async invoice => {
+        let pdf = await createPDFInvoice(invoice);
+        let buffer = Buffer.from(pdf, "base64");
+        let attachment = new MessageAttachment(buffer, `invoice_${invoice.id}.pdf`);
+        const embed = new MessageEmbed()
+            .setTitle("Invoice notified")
+            .setDescription(`Invoice \`id #${invoice.id}\` has been created.`)
+            .setColor("#0099ff")
+            .attachFiles([attachment])
+            .setTimestamp();
         sendWebhook("Invoice created", {
             text: `Invoice \`id #${invoice.id}\` has been created.`,
+            embeds: [embed],
         })
     });
 
-    MainEvent.on("invoice_deleted", invoice => {
+    MainEvent.on("invoice_deleted", async invoice => {
+        let pdf = await createPDFInvoice(invoice);
+        let buffer = Buffer.from(pdf, "base64");
+        let attachment = new MessageAttachment(buffer, `invoice_${invoice.id}.pdf`);
+        const embed = new MessageEmbed()
+            .setTitle("Invoice notified")
+            .setDescription(`Invoice \`id #${invoice.id}\` has been deleted.`)
+            .setColor("#0099ff")
+            .attachFiles([attachment])
+            .setTimestamp();
         sendWebhook("Invoice created", {
             text: `Invoice \`id #${invoice.id}\` has been deleted.`,
+            embeds: [embed],
         })
     });
 
-    MainEvent.on("invoice_updated", invoice => {
+    MainEvent.on("invoice_updated", async invoice => {
+        let pdf = await createPDFInvoice(invoice);
+        let buffer = Buffer.from(pdf, "base64");
+        let attachment = new MessageAttachment(buffer, `invoice_${invoice.id}.pdf`);
+        const embed = new MessageEmbed()
+            .setTitle("Invoice notified")
+            .setDescription(`Invoice \`id #${invoice.id}\` has been updated.`)
+            .setColor("#0099ff")
+            .attachFiles([attachment])
+            .setTimestamp();
         sendWebhook("Invoice updated", {
             text: `Invoice \`id #${invoice.id}\` has been updated.`,
+            embeds: [embed],
         })
     });
 
-    MainEvent.on("invoice_notified", invoice => {
+    MainEvent.on("invoice_notified", async invoice => {
+        let pdf = await createPDFInvoice(invoice);
+        let buffer = Buffer.from(pdf, "base64");
+        let attachment = new MessageAttachment(buffer, `invoice_${invoice.id}.pdf`);
+        const embed = new MessageEmbed()
+            .setTitle("Invoice notified")
+            .setDescription(`Invoice \`id #${invoice.id}\` has been notified.`)
+            .setColor("#0099ff")
+            .attachFiles([attachment])
+            .setTimestamp();
         sendWebhook("Invoice notified", {
             text: `Invoice \`id #${invoice.id}\` has been notified.`,
-        })
+            embeds: [embed],
+        });
     });
 
     MainEvent.on("order_created", order => {
@@ -98,7 +151,10 @@ function getID(url: string)
  * text?: String,
  * }} data 
  */
-function sendWebhook(title: string, data: any)
+function sendWebhook(title: string, data: {
+    embeds?: MessageEmbed[],
+    text?: string,
+})
 {
     const webhookClient = new WebhookClient(getID(discord_webhook_url), getToken(discord_webhook_url));
 
@@ -112,6 +168,7 @@ function sendWebhook(title: string, data: any)
         d.content = data.text;
 
     if(data.embeds)
+        // @ts-ignore
         d.embeds = data.embeds;
 
     webhookClient.send(d);
